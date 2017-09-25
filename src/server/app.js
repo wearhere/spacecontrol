5109a33888102e2739c2bc836ede60bbd1331d1d
Jeff Wear <jeffreywear@gmail.com>
2017-09-24 22:21:12 -0700

    Rename the publication server file for better readability by general audiences

--- src/server/app.js
+++ src/server/app.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
+// Create the web server.
 // Need to wrap the Express server in a real HTTP server for the benefit of the publication server.
 let server = http.createServer(require('./webServer'));
 
-require('./publicationServer')(server);
+// Create the websocket server.
+require('./websocketServer')(server);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, function() {
