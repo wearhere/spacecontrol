4b7acac2ae15398dc1c7eaa259265184512e267b
Stacey Svetlichnaya <staceysv@yahoo-inc.com>
2017-11-03 20:35:22 -0700

    way more controls oops

--- panels/hardware/state.py
+++ panels/hardware/state.py
@@ -7,29 +7,92 @@ from peripherals_stacey import *
 from controls import *
 
 MERINGUE_INPUTS = {
-    "rs_1": Slider(
+    # ROTARY SWITCHES
+    "rs_1_rainbow": Slider(
       device=BLACKPILL,
       pin=0,
       num_bins=6,
       max_val=3950,
       ),
-    "rs_2": Slider(
+    "rs_2_dark_sky": Slider(
       device=BLACKPILL,
       pin=1,
       num_bins=9,
       max_val=3950,
       ),
-    "slide_1": Slider(
+    "rs_3_kardashev": Slider(
       device=BLACKPILL,
       pin=2,
       num_bins=5,
-      max_val=4095,
+      max_val=3950,
       ),
-    "slide_2": Slider(
+    # TOP LIGHT-UP BUTTONS
+    "b_top_blue": Switch(
       device=BLACKPILL,
       pin=3,
+      ),
+    "b_top_white": Switch(
+      device=BLACKPILL,
+      pin=4,
+      ),
+    "b_top_green": Switch(
+      device=BLACKPILL,
+      pin=5,
+      ),   
+    "b_top_yellow": Switch(
+      device=BLACKPILL,
+      pin=6,
+      ),
+    # LEFT SMALL BUTTONS
+    "b_left_blue": Switch(
+      device=BLACKPILL,
+      pin=7,
+      ),
+    "b_left_green": Switch(
+      device=BLACKPILL,
+      pin=8,
+      ),
+    "b_left_black": Switch(
+      device=BLACKPILL,
+      pin=9,
+      ),
+    # SLIDERS
+    "slide_1_temp": Slider(
+      device=BLACKPILL,
+      pin=10,
+      num_bins=5,
+      max_val=4095,
+      ),
+    "slide_2_encounters": Slider(
+      device=BLACKPILL,
+      pin=11,
       num_bins=7,
       max_val=4095,
+      ),
+    # TRIPLE SWITCHES
+    "ts_tongue_1" : Switch(
+      device=BLACKPILL,
+      pin=12,
+      ),
+    "ts_tongue_2" : Switch(
+      device=BLACKPILL,
+      pin=13,
+      ),
+    "ts_light_blue_dick_1" : Switch(
+      device=BLACKPILL,
+      pin=14,
+      ),
+    "ts_light_blue_dick_2" : Switch(
+      device=BLACKPILL,
+      pin=15,
+      ),
+     "ts_big_balls_dick_1" : Switch(
+      device=BLACKPILL,
+      pin=16,
+      ),
+    "ts_big_balls_dick_2" : Switch(
+      device=BLACKPILL,
+      pin=17,
       ),
     }
 
