3a782f1ea2a680f0369e8bda0e330dab19bd2473
Jeff Wear <jeffreywear@gmail.com>
2017-11-04 20:09:17 -0700

    Notify the user that they can accelerate the start timeout

--- src/client/views/TimeToStart.jsx
+++ src/client/views/TimeToStart.jsx
@@ -5,5 +5,10 @@ import Status from '/views/Status';
 export default function TimeToStart(props) {
   if (!_.isNumber(props.time) || (props.time <= 0)) return null;
 
-  return <Status>Game will start in {props.time / 1000}…</Status>;
+  return (
+    <Status>
+      Game will start in {props.time / 1000}…
+      {(props.time > 5000) && <div style={{fontSize: 'smaller'}}>(hit space bar to accelerate)</div>}
+    </Status>
+  );
 }
